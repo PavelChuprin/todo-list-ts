efe1fe5d3eba6682161ee3bc8a3b7b8edc67d21a
Pavel <pcuprin@gmail.com>
2024-04-25 17:15:25 +0600

    fix: sessionStorage

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,13 +7,24 @@ import { RootState, useAppDispatch } from "../store";
 import { Form } from "./Form";
 import { Counter } from "./Counter";
 import { TodoList } from "./TodoList";
+import { ITodo } from "../store/todos/types";
 
 const App: React.FC = () => {
   const items = useSelector((state: RootState) => state.todo.items);
   const dispatch = useAppDispatch();
 
+  let newItems = items;
+  const data = sessionStorage.getItem("items");
+  if (data) {
+    newItems = JSON.parse(data);
+  }
+
   const removeTodo = (id: string): void => {
     dispatch(setItems(items.filter((item) => item.id !== id)));
+    sessionStorage.setItem(
+      "items",
+      JSON.stringify(newItems.filter((newItem: ITodo) => newItem.id !== id))
+    );
   };
 
   const toggleTodo = (id: string): void => {
@@ -25,6 +36,19 @@ const App: React.FC = () => {
           return {
             ...item,
             complete: !item.complete,
+          };
+        })
+      )
+    );
+    sessionStorage.setItem(
+      "items",
+      JSON.stringify(
+        newItems.map((newItem: ITodo) => {
+          if (newItem.id !== id) return newItem;
+
+          return {
+            ...newItem,
+            complete: !newItem.complete,
           };
         })
       )
@@ -49,6 +73,20 @@ const App: React.FC = () => {
         })
       )
     );
+    sessionStorage.setItem(
+      "items",
+      JSON.stringify(
+        newItems.map((newItem: ITodo) => {
+          if (newItem.id !== id) return newItem;
+
+          return {
+            ...newItem,
+            title: newTitle,
+            description: newDescription,
+          };
+        })
+      )
+    );
   };
 
   return (
@@ -60,9 +98,9 @@ const App: React.FC = () => {
       }}
     >
       <Title level={4}>Todo List</Title>
-      <Form items={items} />
+      <Form items={newItems} />
       <TodoList
-        items={items}
+        items={newItems}
         removeTodo={removeTodo}
         toggleTodo={toggleTodo}
         saveTodo={saveTodo}
@@ -74,7 +112,7 @@ const App: React.FC = () => {
           padding: 16,
         }}
       >
-        {items.length > 4 && <Counter items={items} />}
+        {newItems.length > 4 && <Counter items={newItems} />}
         <FloatButton.BackTop
           style={{
             right: 24,
